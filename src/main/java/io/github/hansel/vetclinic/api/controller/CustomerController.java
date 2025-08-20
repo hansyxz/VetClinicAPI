ffb0a4d3de32911719d5d6b2f75d7b6c0cafb58e
Hansel <fernandohanselsc@gmail.com>
2025-08-20 17:00:14 -0300

    feat: adicionar CustomerController e CustomerService

---
+++ src/main/java/io/github/hansel/vetclinic/api/controller/CustomerController.java
@@ -0,0 +1,40 @@
+package io.github.hansel.vetclinic.api.controller;
+
+import io.github.hansel.vetclinic.api.dto.request.CustomerRequest;
+import io.github.hansel.vetclinic.api.dto.response.CustomerSummaryResponse;
+import io.github.hansel.vetclinic.api.service.CustomerService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @Autowired
+    public CustomerService service;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity create(@RequestBody @Valid CustomerRequest request, UriComponentsBuilder uriBuilder){
+        var response = service.create(request);
+        var uri = uriBuilder.path("/customer/{id}").buildAndExpand(response.id()).toUri();
+
+        return ResponseEntity.created(uri).body(response);
+    }
+
+    @GetMapping
+    @Transactional(readOnly = true)
+    public ResponseEntity<Page<CustomerSummaryResponse>> findAll(@PageableDefault(size = 10, sort = {"id"}) Pageable pageable) {
+        var page = service.findAll(pageable);
+
+        return ResponseEntity.ok(page);
+    }
+
+}
---
+++ src/main/java/io/github/hansel/vetclinic/api/service/CustomerService.java
@@ -0,0 +1,29 @@
+package io.github.hansel.vetclinic.api.service;
+
+import io.github.hansel.vetclinic.api.dto.request.CustomerRequest;
+import io.github.hansel.vetclinic.api.dto.response.CustomerDetailResponse;
+import io.github.hansel.vetclinic.api.dto.response.CustomerSummaryResponse;
+import io.github.hansel.vetclinic.api.entity.Customer;
+import io.github.hansel.vetclinic.api.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    public CustomerRepository repository;
+
+    public CustomerDetailResponse create(CustomerRequest request) {
+        var customer = new Customer(request);
+        repository.save(customer);
+
+        return new CustomerDetailResponse(customer);
+    }
+
+    public Page<CustomerSummaryResponse> findAll(Pageable pageable) {
+        return repository.findAll(pageable).map(CustomerSummaryResponse::new);
+    }
+}
