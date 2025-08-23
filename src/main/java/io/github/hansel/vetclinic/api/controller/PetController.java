a83819a36be6b33789de446d6176d1cbc1f13d9f
Hansel <fernandohanselsc@gmail.com>
2025-08-23 15:39:58 -0300

    feat: implementar findById e findAll no Pet

--- src/main/java/io/github/hansel/vetclinic/api/controller/PetController.java
+++ src/main/java/io/github/hansel/vetclinic/api/controller/PetController.java
@@ -1,15 +1,19 @@
 package io.github.hansel.vetclinic.api.controller;
 
 import io.github.hansel.vetclinic.api.dto.request.PetRequest;
+import io.github.hansel.vetclinic.api.dto.response.CustomerDetailResponse;
+import io.github.hansel.vetclinic.api.dto.response.CustomerSummaryResponse;
+import io.github.hansel.vetclinic.api.dto.response.PetDetailResponse;
+import io.github.hansel.vetclinic.api.dto.response.PetSummaryResponse;
 import io.github.hansel.vetclinic.api.service.PetService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -26,5 +30,17 @@ public class PetController {
         var uri = uriBuilder.path("/pet/{id}").buildAndExpand(response.id()).toUri();
 
         return ResponseEntity.created(uri).body(response);
+    }
+
+    @GetMapping
+    @Transactional(readOnly = true)
+    public ResponseEntity<Page<PetSummaryResponse>> findAll(@PageableDefault(size = 10, sort = {"id"}) Pageable pageable) {
+        return ResponseEntity.ok(service.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    @Transactional(readOnly = true)
+    public ResponseEntity<PetDetailResponse> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
     }
 }
--- src/main/java/io/github/hansel/vetclinic/api/service/PetService.java
+++ src/main/java/io/github/hansel/vetclinic/api/service/PetService.java
@@ -1,7 +1,8 @@
 package io.github.hansel.vetclinic.api.service;
 
 import io.github.hansel.vetclinic.api.dto.request.PetRequest;
-import io.github.hansel.vetclinic.api.dto.response.PetResponse;
+import io.github.hansel.vetclinic.api.dto.response.PetDetailResponse;
+import io.github.hansel.vetclinic.api.dto.response.PetSummaryResponse;
 import io.github.hansel.vetclinic.api.entity.Pet;
 import io.github.hansel.vetclinic.api.entity.enums.Species;
 import io.github.hansel.vetclinic.api.exception.BadRequestException;
@@ -9,6 +10,8 @@ import io.github.hansel.vetclinic.api.exception.NotFoundException;
 import io.github.hansel.vetclinic.api.repository.CustomerRepository;
 import io.github.hansel.vetclinic.api.repository.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -17,24 +20,33 @@ import java.math.BigDecimal;
 public class PetService {
 
     @Autowired
-    public PetRepository petRepository;
+    public PetRepository repository;
 
     @Autowired
     public CustomerRepository customerRepository;
 
-    public PetResponse create(PetRequest request) {
+    public PetDetailResponse create(PetRequest request) {
 
         var customer = customerRepository.findById(request.ownerId())
                 .orElseThrow(() -> new NotFoundException("Customer not found with id " + request.ownerId()));
 
         validateSpecies(request);
-        validateAge(request.age());
-        validateWeight(request.weightKg());
 
         var pet = new Pet(request, customer);
-        petRepository.save(pet);
+        repository.save(pet);
 
-        return new PetResponse(pet);
+        return new PetDetailResponse(pet);
+    }
+
+    public Page<PetSummaryResponse> findAll(Pageable pageable) {
+        return repository.findAll(pageable).map(PetSummaryResponse::new);
+    }
+
+    public PetDetailResponse findById(Long id) {
+        var pet = repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Pet not found with id " + id));
+
+        return new PetDetailResponse(pet);
     }
 
     private void validateSpecies(PetRequest request) {
@@ -44,26 +56,6 @@ public class PetService {
 
         if (request.species() != Species.OTHER && request.otherSpecies() != null) {
             throw new BadRequestException("The field 'otherSpecies' can only be used when species is set to OTHER");
-        }
-    }
-
-    private void validateAge(Integer age) {
-        if (age < 0) {
-            throw new BadRequestException("Age cannot be negative");
-        }
-        if (age > 120) {
-            throw new BadRequestException("Age cannot exceed 120 years");
-        }
-    }
-
-    private void validateWeight(BigDecimal weightKg) {
-        if (weightKg != null) {
-            if (weightKg.doubleValue() <= 0) {
-                throw new BadRequestException("Weight must be greater than zero");
-            }
-            if (weightKg.doubleValue() > 100) {
-                throw new BadRequestException("Weight cannot exceed 100 kg");
-            }
         }
     }
 }
