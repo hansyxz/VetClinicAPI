7c4fbda244f97bc8cd4c27bbdff5bb54e543abc2
Hansel <fernandohanselsc@gmail.com>
2025-09-06 15:56:00 -0300

    feat: adicionar DTOs de response para Appointment, AppointmentItem e Offering

--- src/main/java/io/github/hansel/vetclinic/api/dto/response/AppointmentItemResponse.java
+++ src/main/java/io/github/hansel/vetclinic/api/dto/response/AppointmentItemResponse.java
@@ -1,2 +1,13 @@
-package io.github.hansel.vetclinic.api.dto.response;public record AppointmentItemResponse() {
+package io.github.hansel.vetclinic.api.dto.response;
+
+import io.github.hansel.vetclinic.api.entity.AppointmentItem;
+
+import java.math.BigDecimal;
+
+public record AppointmentItemResponse(Long id, AppointmentSummaryResponse appointment, OfferingResponse offering,
+                                      EmployeeSummaryResponse employee, int amount, BigDecimal unitPrice) {
+    public AppointmentItemResponse(AppointmentItem appointmentItem) {
+        this(appointmentItem.getId(), new AppointmentSummaryResponse(appointmentItem.getAppointment()), new OfferingResponse(appointmentItem.getOffering()),
+                new EmployeeSummaryResponse(appointmentItem.getEmployee()), appointmentItem.getAmount(), appointmentItem.getUnitPrice());
+    }
 }
--- src/main/java/io/github/hansel/vetclinic/api/dto/response/OfferingResponse.java
+++ src/main/java/io/github/hansel/vetclinic/api/dto/response/OfferingResponse.java
@@ -1,2 +1,12 @@
-package io.github.hansel.vetclinic.api.dto.response;public record OfferingResponse() {
+package io.github.hansel.vetclinic.api.dto.response;
+
+import io.github.hansel.vetclinic.api.entity.Offering;
+import io.github.hansel.vetclinic.api.entity.enums.Category;
+
+import java.math.BigDecimal;
+
+public record OfferingResponse(Long id, String name, String description, Category category, BigDecimal price) {
+    public OfferingResponse(Offering offering) {
+        this(offering.getId(), offering.getName(), offering.getDescription(), offering.getCategory(), offering.getPrice());
+    }
 }
